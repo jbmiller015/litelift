c7718f288018f3c505ef9f70498e8c508c283735
Jacob Miller <jbmiller015@gmail.com>
2024-09-18 21:43:56 -0400

    Commented data example for local development

--- src/api/exercise.tsx
+++ src/api/exercise.tsx
@@ -3,7 +3,9 @@ import {NextApiRequest, NextApiResponse} from "next";
 import {ObjectId} from "bson";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-
+    /**
+     * {"_id":{"$oid":"66e0fee187e6536421d071f4"},"user_id":{"$oid":"66e0fff387e6536421d071f6"},"w_r":[{"weight":{"$numberDouble":"30.5"},"reps":{"$numberInt":"3"},"complete":false,"fail":false},{"weight":{"$numberDouble":"42.0"},"reps":{"$numberInt":"4"},"complete":false,"fail":false}],"name":"Squat"}
+     */
     try {
         const data = await clientPromise;
         const db = data.db(process.env.DB_NAME);
