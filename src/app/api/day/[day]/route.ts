f4c93541eb2e17156bb515b8f6e8a05b1ae7b4fd
jbmil <jbmiller015@gmail.com>
2024-10-21 09:06:56 -0400

    Starting spec lift edit pages

--- src/app/api/day/[day]/route.ts
+++ src/app/api/day/[day]/route.ts
@@ -9,7 +9,6 @@ export async function GET(request: Request) {
     const cookieStore = cookies();
     cookieStore.set(headerCookie[0], headerCookie[1]);
     const token = cookieStore.get('token')?.value;
-
     try {
         if (token && token !== 'demo') {
             const payload = await jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
@@ -24,22 +23,74 @@ export async function GET(request: Request) {
                 const data = await clientPromise;
                 const db = data.db(process.env.DB_NAME);
                 const userIdObject = new ObjectId(userId);
-                const agg = [
-                    {
-                        '$match': {
-                            'user_id': userIdObject
+                const day = await db.collection(process.env.DAY_COL).find({user_id: userIdObject}).toArray();
+                return Response.json(day, {statusText: "success", status: 200});
+            } catch (e) {
+                console.log(e)
+                return Response.json('Unexpected error occurred.', {statusText: "Error", status: 422});
+            }
+        }
+    } catch (err) {
+        console.log(err)
+        return Response.json('Unexpected error occurred.', {statusText: "Error", status: 422});
+    }
+
+}
+
+export async function PUT(request: Request) {
+    const headerCookie = request.headers.get('cookie').split('=');
+    const cookieStore = cookies();
+    cookieStore.set(headerCookie[0], headerCookie[1]);
+    const token = cookieStore.get('token')?.value;
+    try {
+        if (token && token !== 'demo') {
+            const payload = await jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
+                if (err) {
+                    console.log(err)
+                    return Response.json('You must be logged in.', {statusText: "Error", status: 401});
+                }
+                return payload.payload;
+            });
+            const {userId} = payload;
+            const userIdObject = new ObjectId(userId);
+            try {
+                const reqData = await request.json();
+                const deleteData = reqData.delete_data;
+                const editData = reqData.edit_data;
+                const payload = editData.map((el) => {
+                    if (!el.user_id) {
+                        el.user_id = userIdObject;
+                    } else if (el.user_id) {
+                        el.user_id = new ObjectId(el.user_id);
+                    }
+                    if (el._id) {
+                        el._id = new ObjectId(el._id);
+                    }
+                    return el;
+                })
+                let bulkOps = payload.map(el => {
+                    return el._id ? {
+                        updateOne: {
+                            "filter": {_id: el._id, user_id: el.user_id},
+                            "update": {$set: {name: el.name, exerciseData: el.exerciseData}},
+                            "upsert": true
                         }
-                    }, {
-                        '$lookup': {
-                            'from': 'exercise',
-                            'localField': 'exerciseData',
-                            'foreignField': '_id',
-                            'as': 'exerciseData'
+                    } : {
+                        insertOne: {"document": {name: el.name, exerciseData: el.exerciseData, user_id: el.user_id}}
+                    }
+                })
+                bulkOps.push(...deleteData.map((el) => {
+                    const _id = new ObjectId(el);
+                    return {
+                        deleteOne: {
+                            "filter": {_id},
                         }
                     }
-                ];
-                const day = await db.collection('day').aggregate(agg).toArray();
-                return Response.json(day[0], {statusText: "success", status: 200});
+                }))
+                const data = await clientPromise;
+                const db = data.db(process.env.DB_NAME);
+                const day = await db.collection(process.env.DAY_COL).bulkWrite(bulkOps);
+                return Response.json(day, {statusText: "success", status: 200});
             } catch (e) {
                 console.log(e)
                 return Response.json('Unexpected error occurred.', {statusText: "Error", status: 422});
